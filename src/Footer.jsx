9e429a0d7fa6832470aeb359ac6417e5c9a3d1c5
zoyron <sgrarora3@gmail.com>
2024-07-18 00:37:43 +0530

    corrected the url

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div>
       {'To see the source code'}
-      < a href="https:/github.com/zoyron/dad-joke-generator" target="_blank"> click here</a >
+      < a href="https://github.com/zoyron/dad-joke-generator" target="_blank"> click here</a >
     </div>
   );
 }
