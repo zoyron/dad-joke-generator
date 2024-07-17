5646010b85c70715a3f8fbf904000be377fd696a
zoyron <sgrarora3@gmail.com>
2024-07-17 23:03:56 +0530

    somewhat done

--- src/Dadjokes.jsx
+++ src/Dadjokes.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { useState } from "react";
 import Button from "./Button";
+import { getDadJoke } from "./services/jokeService";
 
 export default function Dadjokes() {
+  const [joke, setJoke] = useState('Click on the button to generate a Dad Joke')
+  const generateJoke = () {
+    try {
+      const newJoke = await getDadJoke();
+      setJoke(newJoke);
+    } catch (error) {
+      console.log(error);
+      setJoke('Failed to get a joke, please try again!');
+    }
+  }
+
   return <>
+    <h2>{joke}</h2>
     <Button onClick={generateJoke} text="Generate" />
   </>
 }
