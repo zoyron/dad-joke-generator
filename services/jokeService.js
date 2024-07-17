5f1b104201575501ec56c06691079e2ef473936d
zoyron <sgrarora3@gmail.com>
2024-07-17 22:52:18 +0530

    added: functionality to dadJoke service

---
+++ services/jokeService.js
@@ -0,0 +1,16 @@
+export async function getDadJoke() {
+  const response = await fetch("https://icanhazdadjoke.com/", {
+    headers: {
+      accept: 'application/json',
+      accept: 'text/plain',
+      'User-Agent': 'My Library',
+    }
+  });
+
+  if (!response.ok) {
+    throw new Error(`Error Statur: ${response.status}`);
+  }
+
+  const dadJoke = await response.json();
+  return dadJoke.joke;
+}
