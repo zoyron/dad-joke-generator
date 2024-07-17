929c697f6b4b03ef33f8d1ca9ecb359de6f196de
zoyron <sgrarora3@gmail.com>
2024-07-17 22:31:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import DadJokes from "./Dadjokes";
+import "./App.css";
+
+export default function App() {
+
+  return (
+    <>
+      <h1>Get a Dad Joke</h1>
+      <DadJokes />
+    </>
+  )
+}
